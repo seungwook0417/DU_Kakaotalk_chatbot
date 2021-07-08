cc04871c25e96deef88aa04b7fb24ddd4afba45c
seungwook0417 <68508835+seungwook0417@users.noreply.github.com>
2021-07-08 19:50:04 +0900

    2021.07.08 서비스 종료

--- main.py
+++ main.py
@@ -2,37 +2,24 @@
 from flask import Flask, request, jsonify
 
 # Internal Module
-from setting.card import *  # 카카오톡 데이터 전송 양식
-from setting.answer_main import *  # 퀵 리플라이 지정
-from haksa.haksa_nofi import *  # 학사공지 검색
 from haksa.haksa_schedule import *  # 학사일정 검색
 from haksa.find_lecture_room import *  # 강의실 검색
 from haksa.find_member import *  # 구성원 검색
-from haksa.find_page import *  # 학과 페이지 검색
 from useful.bus_station import *  # 버스정류장 검색
 from useful.error_report import *  # 오류보고 보고서
+
+# 2021.07.08 서비스 종료
+from setting.card import *  # 카카오톡 데이터 전송 양식
+from setting.answer_main import *  # 퀵 리플라이 지정
+from haksa.haksa_nofi import *  # 학사공지 검색
+from haksa.find_page import *  # 학과 페이지 검색
 from useful.weather_paser import *  # 날씨 검색
 from useful.facilities import *
 
 app = Flask(__name__)
 
-
 # Route urls
 # app.route는 flask의 기능으로, '/hello'와 같은 url로 요청이 들어오면 해당 함수만 호출한다.
-
-# 환영인사 작동 이식예정
-@app.route("/hello", methods=['POST'])
-def hello():
-    response = answer()
-    return jsonify(response)
-
-
-# 학사공지
-@app.route("/haksa", methods=['POST'])
-def haksa():
-    response = haksa_Parser()
-    return jsonify(response)
-
 
 # 학사일정
 @app.route("/schedule", methods=['POST'])
@@ -41,6 +28,12 @@ def schedule():
     response = haksa_chedule_Parser(content)
     return jsonify(response)
 
+# 버스정류장 검색
+@app.route("/find_bus", methods=['POST'])
+def find_bus():
+    content = request.get_json()
+    response = find_bus_Paser(content)
+    return jsonify(response)
 
 # 강의실 검색
 @app.route("/find_lecture", methods=['POST'])
@@ -49,21 +42,11 @@ def find_lecture():
     response = lecture_Parser(content)
     return jsonify(response)
 
-
-# 편의시설 검색
-@app.route("/find_building", methods=['POST'])
-def find_building():
-    content = request.get_json()
-    response = facilities_parser(content)
-    return jsonify(response)
-
-
 # 구성원 검색
 @app.route("/search", methods=['POST'])
 def search():
     response = search_Parser()
     return jsonify(response)
-
 
 # 이름 연락처 찾기
 @app.route("/search_member", methods=['POST'])
@@ -71,7 +54,6 @@ def search_member():
     content = request.get_json()
     response = search_member_Parser(content)
     return jsonify(response)
-
 
 # 소속 연락처 찾기
 @app.route("/search_buseo", methods=['POST'])
@@ -87,22 +69,6 @@ def search_telno():
     response = search_telno_Parser(content)
     return jsonify(response)
 
-# 학과 페이지 검색
-@app.route("/page", methods=['POST'])
-def page():
-    content = request.get_json()
-    response = search_page_Parser(content)
-    return jsonify(response)
-
-
-# 버스정류장 검색
-@app.route("/find_bus", methods=['POST'])
-def find_bus():
-    content = request.get_json()
-    response = find_bus_Paser(content)
-    return jsonify(response)
-
-
 # 오류보고 보고서
 @app.route('/message', methods=['POST'])
 def message():
@@ -111,11 +77,31 @@ def message():
     return jsonify(response)
 
 
-# 날씨 검색
-@app.route('/weather', methods=['POST'])
-def weather():
-    response = naver_weather_parser()
-    return jsonify(response)
+# 학사공지 2021.07.08 서비스 종료
+# @app.route("/haksa", methods=['POST'])
+# def haksa():
+#     response = haksa_Parser()
+#     return jsonify(response)
+
+# 편의시설 검색 2021.07.08 서비스 종료
+# @app.route("/find_building", methods=['POST'])
+# def find_building():
+#     content = request.get_json()
+#     response = facilities_parser(content)
+#     return jsonify(response)
+
+# 학과 페이지 검색 2021.07.08 서비스 종료
+# @app.route("/page", methods=['POST'])
+# def page():
+#     content = request.get_json()
+#     response = search_page_Parser(content)
+#     return jsonify(response)
+
+# 날씨 검색 2021.07.08 서비스 종료
+# @app.route('/weather', methods=['POST'])
+# def weather():
+#     response = naver_weather_parser()
+#     return jsonify(response)
 
 
 if __name__ == "__main__":
--- useful/prof_restaurant.py
+++ useful/prof_restaurant.py
@@ -1,40 +1,41 @@
 ##교직원 식당 추가기능
-import json
-import time
-from setting.card import *
-from setting.answer_main import answer
-
-
-# 교직원 식당 식단 출력
-def pf_restaurant():
-    # 식단 데이터 접근
-    json_data = open('pf_restaurant.json', 'r', encoding="utf-8").read()
-    data = json.loads(json_data)
-
-    date = time.strftime('%Y-%m-%d', time.localtime(time.time()))
-
-    # 매일 변경되는 교직원 식당 한정
-    restaurant = ["복지관", "웅지관"]
-
-    response = {'version': '2.0', 'template': {
-        'outputs': [{"simpleText": {"text": "오늘의 식단"}},
-                    {"carousel": {"type": "basicCard", "items": []}}], 'quickReplies': []}}
-
-    # 리스트 삽입 및 출력 코드
-    for i in restaurant:
-        try:
-            a = data[str(date)][i]
-            price = a["price"]
-            menu_total = []
-            if a['menu'] != []:
-                for menu in a['menu']:
-                    menu_total.append(menu)
-            else:
-                menu_total.append("오늘 등록된 식단이 없습니다.")
-
-            menu = '\n'.join(str(e) for e in menu_total)
-            response = insert_carousel_card(response, i + "식단(" + price + ")", menu)
-            response = answer(response)
-        except:
-            continue
-    return response
+# 개발 중단
+# import json
+# import time
+# from setting.card import *
+# from setting.answer_main import answer
+#
+#
+# # 교직원 식당 식단 출력
+# def pf_restaurant():
+#     # 식단 데이터 접근
+#     json_data = open('pf_restaurant.json', 'r', encoding="utf-8").read()
+#     data = json.loads(json_data)
+#
+#     date = time.strftime('%Y-%m-%d', time.localtime(time.time()))
+#
+#     # 매일 변경되는 교직원 식당 한정
+#     restaurant = ["복지관", "웅지관"]
+#
+#     response = {'version': '2.0', 'template': {
+#         'outputs': [{"simpleText": {"text": "오늘의 식단"}},
+#                     {"carousel": {"type": "basicCard", "items": []}}], 'quickReplies': []}}
+#
+#     # 리스트 삽입 및 출력 코드
+#     for i in restaurant:
+#         try:
+#             a = data[str(date)][i]
+#             price = a["price"]
+#             menu_total = []
+#             if a['menu'] != []:
+#                 for menu in a['menu']:
+#                     menu_total.append(menu)
+#             else:
+#                 menu_total.append("오늘 등록된 식단이 없습니다.")
+#
+#             menu = '\n'.join(str(e) for e in menu_total)
+#             response = insert_carousel_card(response, i + "식단(" + price + ")", menu)
+#             response = answer(response)
+#         except:
+#             continue
+#     return response
